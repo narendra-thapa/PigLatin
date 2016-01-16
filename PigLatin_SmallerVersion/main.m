56c687a9ad89fe8bfd0df461c6a168cf43c7a2e9
narendrathapa <thapa.narendra@gmail.com>
2016-01-15 19:23:00 -0500

    Initial Commit

---
+++ PigLatin_SmallerVersion/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin_SmallerVersion
+//
+//  Created by Narendra Thapa on 2016-01-15.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
